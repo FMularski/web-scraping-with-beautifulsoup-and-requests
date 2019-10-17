abe54331553a308ab9e62ba915258926c0375715
Filip Mularski <filipelblag@gmail.com>
2019-10-17 18:38:49 +0200

    opening html file to create beautifulsoup obj, basic printing tags

--- scrape.py
+++ scrape.py
@@ -1,4 +1,13 @@
 # pip install beautifulsoup4
 # pip install requests
-# pip install xmxl
+# pip install xmxl --> a html code parser
 
+from bs4 import BeautifulSoup
+import requests
+
+with open('index.html') as html_file:
+    soup = BeautifulSoup(html_file, 'lxml') #beautifulsoup object ( args: web page file and parser)
+
+# print(soup.prettify())  # prints html code
+# print(soup.title) # prints <title> title text </title>
+# print(soup.title.text) # prints title text
